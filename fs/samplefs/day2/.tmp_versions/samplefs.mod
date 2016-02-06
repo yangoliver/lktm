c4bc926ed1361aa0100ab393985b63e2266aaaa0
Yong Yang <yangoliver@gmail.com>
2016-02-06 13:36:45 +0800

    ported day1 code changes for new kernel

--- fs/samplefs/day2/.tmp_versions/samplefs.mod
+++
@@ -1,2 +0,0 @@
-/home/stevef/s-fs-scratch/day2/samplefs.ko
-/home/stevef/s-fs-scratch/day2/super.o
--- fs/samplefs/day2/super.c
+++ fs/samplefs/day2/super.c
@@ -138,7 +138,13 @@ static int samplefs_fill_super(struct super_block *sb, void *data, int silent)
 	return 0;
 }
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,18)
+#if LINUX_VERSION_CODE > KERNEL_VERSION(2,6,39)
+static struct dentry *samplefs_mount(struct file_system_type *fs_type, int flags,
+	const char *dev_name, void *data)
+{
+	return mount_nodev(fs_type, flags, data, samplefs_fill_super);
+}
+#elif LINUX_VERSION_CODE < KERNEL_VERSION(2,6,18)
 struct super_block * samplefs_get_sb(struct file_system_type *fs_type,
 	int flags, const char *dev_name, void *data)
 {
@@ -156,7 +162,11 @@ int samplefs_get_sb(struct file_system_type *fs_type,
 static struct file_system_type samplefs_fs_type = {
 	.owner = THIS_MODULE,
 	.name = "samplefs",
+#if LINUX_VERSION_CODE > KERNEL_VERSION(2,6,39)
+	.mount = samplefs_mount,
+#else
 	.get_sb = samplefs_get_sb,
+#endif
 	.kill_sb = kill_anon_super,
 	/*  .fs_flags */
 };
@@ -174,4 +184,4 @@ static void __exit exit_samplefs_fs(void)
 
 module_init(init_samplefs_fs)
 module_exit(exit_samplefs_fs)
-
+MODULE_LICENSE("GPL");
