43bb247badf9b8b8851d78216bdc596604f5b908
Yong Yang <yangoliver@gmail.com>
2016-04-19 08:20:41 -0700

    add disk alloc code and make it pass the module build
    Signed-off-by: Yong Yang <yangoliver@gmail.com>

--- drivers/block/sampleblk/day1/sample_blk.c
+++ drivers/block/sampleblk/day1/sample_blk.c
@@ -23,14 +23,82 @@
 #include <linux/module.h>
 #include <linux/version.h>
 #include <linux/fs.h>
+#include <linux/slab.h>
+#include <linux/blkdev.h>
 
 static int sampleblk_major;
+#define SAMPLEBLK_MINOR	1
+
+struct sampleblk_dev {
+	int minor;
+	spinlock_t lock;
+	struct request_queue *queue;
+	struct gendisk *disk;
+};
+
+struct sampleblk_dev *sampleblk_dev = NULL;
+
+static int sampleblk_alloc(int minor)
+{
+	struct gendisk *disk;
+	int rv = 0;
+
+	sampleblk_dev = kzalloc(sizeof(sampleblk_dev), GFP_KERNEL);
+	if (!sampleblk_dev) {
+		rv = ENOMEM;
+		goto fail;
+	}
+
+	sampleblk_dev->minor = minor;
+
+	spin_lock_init(&sampleblk_dev->lock);
+
+	sampleblk_dev->queue = blk_alloc_queue(GFP_KERNEL);
+	if (!sampleblk_dev->queue) {
+		rv = ENOMEM;
+		goto fail_dev;
+	}
+
+	disk = alloc_disk(minor);
+	if (!disk) {
+		rv = ENOMEM;
+		goto fail_queue;
+	}
+
+	disk->major = sampleblk_major;
+	disk->first_minor   = minor;
+	disk->fops      = NULL;
+	disk->private_data  = sampleblk_dev;
+	disk->queue     = sampleblk_dev->queue;
+	sprintf(disk->disk_name, "sampleblk%d", minor);
+
+	return 0;
+
+fail_queue:
+	blk_cleanup_queue(sampleblk_dev->queue);
+fail_dev:
+	kfree(sampleblk_dev);
+fail:
+	return rv;
+}
+
+static void sampleblk_free(struct sampleblk_dev *sampleblk_dev)
+{
+	blk_cleanup_queue(sampleblk_dev->queue);
+	kfree(sampleblk_dev);
+}
 
 static int __init sampleblk_init(void)
 {
+	int rv = 0;
+
 	sampleblk_major = register_blkdev(0, "sampleblk");
 	if (sampleblk_major < 0)
 		return sampleblk_major;
+
+	rv = sampleblk_alloc(SAMPLEBLK_MINOR);
+	if (rv < 0)
+		pr_info("sampleblk: disk allocation failed with %d\n", rv);
 
 	pr_info("sampleblk: module loaded\n");
 	return 0;
@@ -38,6 +106,7 @@ static int __init sampleblk_init(void)
 
 static void __exit sampleblk_exit(void)
 {
+	sampleblk_free(sampleblk_dev);
 	unregister_blkdev(sampleblk_major, "sampleblk");
 
 	pr_info("sampleblk: module unloaded\n");
