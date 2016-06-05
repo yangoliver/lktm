d0d27a7fd8aa61fd530b7d1c014a083dab23712f
Yong Yang <yangoliver@gmail.com>
2016-06-04 21:57:54 -0700

    umount warnings caused by missing unlock_new_inode call
    Signed-off-by: Yong Yang <yangoliver@gmail.com>

--- fs/samplefs/day2/super.c
+++ fs/samplefs/day2/super.c
@@ -124,6 +124,8 @@ static int samplefs_fill_super(struct super_block *sb, void *data, int silent)
 	if (!inode)
 		return -ENOMEM;
 
+	unlock_new_inode(inode);
+
 	sb->s_fs_info = kzalloc(sizeof(struct samplefs_sb_info), GFP_KERNEL);
 	sfs_sb = SFS_SB(sb);
 	if (!sfs_sb) {
